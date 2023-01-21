93f249374bdff71a767e17ed6a8acc2c5d631748
rosauda <rodrigo.sauda@hotmail.com>
2023-01-21 20:25:43 +0100

    Starting app

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=300, height=100)
+window.config(padx=5, pady=5)
+
+
+
+
+
+window.mainloop()
